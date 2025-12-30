dae3690a2d331bfb5ecf4763967dc662979d2533
Lucius7nya <3012967200@qq.com>
2025-12-30 20:05:25 +0800

    Remove copyright section from PostCopyright component
    Removed copyright section rendering in PostCopyright component.

--- src/components/modules/post/PostCopyright.tsx
+++ src/components/modules/post/PostCopyright.tsx
@@ -27,29 +27,5 @@ export const PostCopyright: FC = () => {
   if (!data) return null
   const { title, link, date } = data
   return (
-    <section
-      className="mt-4 text-sm leading-loose text-gray-600 dark:text-neutral-400"
-      id="copyright"
-    >
-      <p>文章标题：{title}</p>
-      <p>文章作者：{name}</p>
-      <p>
-        文章链接：<span>{link}</span>{' '}
-        <a
-          onClick={() => {
-            navigator.clipboard.writeText(link)
-            toast.success('已复制文章链接')
-          }}
-          data-hide-print
-          className="cursor-pointer select-none"
-        >
-          [复制]
-        </a>
-      </p>
-      <p>
-        最后修改时间:{' '}
-        {date ? dayjs(date).format('YYYY 年 MM 月 DD 日 H:mm') : '暂没有修改过'}
-      </p>
-    </section>
   )
 }
