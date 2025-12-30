e2fdf69258e996bad9639bff0bcc1e72356af62a
Lucius7nya <3012967200@qq.com>
2025-12-30 20:00:23 +0800

    Remove copyright notice from PostCopyright component
    Removed copyright notice and related text from PostCopyright component.

--- src/components/modules/post/PostCopyright.tsx
+++ src/components/modules/post/PostCopyright.tsx
@@ -50,21 +50,6 @@ export const PostCopyright: FC = () => {
         最后修改时间:{' '}
         {date ? dayjs(date).format('YYYY 年 MM 月 DD 日 H:mm') : '暂没有修改过'}
       </p>
-      <Divider />
-      <div>
-        <p>
-          商业转载请联系站长获得授权，非商业转载请注明本文出处及文章链接，您可以自由地在任何媒体以任何形式复制和分发作品，也可以修改和创作，但是分发衍生作品时必须采用相同的许可协议。
-          <br />
-          本文采用
-          <a
-            className="shiro-link--underline"
-            href="https://creativecommons.org/licenses/by-nc-sa/4.0/"
-          >
-            CC BY-NC-SA 4.0 - 非商业性使用 - 相同方式共享 4.0 国际
-          </a>
-          进行许可。
-        </p>
-      </div>
     </section>
   )
 }
